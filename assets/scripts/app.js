729096acfc311c75fdadad371c90fadad1d6569f
Zykie Rallos <zykie1018@gmail.com>
2023-08-23 19:39:43 +0800

    Feature: revisit arrays and methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,26 @@
-const user = {
-    "name": "Max",
-    "age": 14,
-    greet() {
-        console.log("Hello!");
-        console.log(this.name);
-    }
-}
+// const user = {
+//     "name": "Max",
+//     "age": 14,
+//     greet() {
+//         console.log("Hello!");
+//         console.log(this.name);
+//     }
+// }
 
-console.log(user.name);
+// console.log(user.name);
 
-user.greet();
+// user.greet();
+
+const hobbies = ["sports", "cooking", "reading"];
+
+hobbies.push("Kayaking");
+console.log(hobbies[0]);
+
+const index = hobbies.findIndex((item) => item === "sports")
+
+console.log(index);
+
+// Wrap object in a parenthesis
+const newArray = hobbies.map((item) => ({ text: item, isReading: true, }));
+
+console.log(newArray);
