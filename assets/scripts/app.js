68c8a81b174d28adad7a8db962434fcf8b81c76b
Zykie Rallos <zykie1018@gmail.com>
2023-08-25 08:17:48 +0800

    Feature: revisit destructuring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,16 +11,35 @@
 
 // user.greet();
 
-const hobbies = ["sports", "cooking", "reading"];
+// const hobbies = ["sports", "cooking", "reading"];
 
-hobbies.push("Kayaking");
-console.log(hobbies[0]);
+// hobbies.push("Kayaking");
+// console.log(hobbies[0]);
 
-const index = hobbies.findIndex((item) => item === "sports")
+// const index = hobbies.findIndex((item) => item === "sports")
 
-console.log(index);
+// console.log(index);
 
-// Wrap object in a parenthesis
-const newArray = hobbies.map((item) => ({ text: item, isReading: true, }));
+// // Wrap object in a parenthesis
+// const newArray = hobbies.map((item) => ({ text: item, isReading: true, }));
 
-console.log(newArray);
+// console.log(newArray);
+
+// DESTRUCTURING
+// const [firstName, lastName] = ["Max", "Smith"]
+
+// const firstName = userNameData[0]
+// const lastName = userNameData[1]
+
+// console.log(firstName + lastName);
+// console.log(firstName);
+// console.log(lastName);
+
+// pulled out by property name
+const { name: userName, age: userAge } = {
+    "name": "John",
+    "age": 33,
+}
+
+console.log(userName);
+console.log(userAge);
